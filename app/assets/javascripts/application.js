f30c0b5f4f282e9637ea1dddd0ee7f46effee6e1
ykhanfar <43030927+ykhanfar@users.noreply.github.com>
2022-10-19 13:05:21 +0300

    Added styling to add and remove publication buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require jquery
-//= require cocoon
 //= require activestorage
 //= require bootstrap-sprockets
 //= require bootstrap-table/bootstrap-table
 //= require_tree .
+//= require cocoon
 
 //= require add_author_fields
