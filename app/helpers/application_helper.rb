7671a9143327bbe0c8f001a6e4bad354f783086e
ykhanfar <43030927+ykhanfar@users.noreply.github.com>
2022-10-19 03:09:30 +0300

    Stylized nav-bar and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_is_active(controller)
+    params[:controller] == controller ? "nav-link active" : "nav-link"
+  end
+
+  def indicate_current(controller)
+    params[:controller] == controller ? "aria-current=\"page\"" : nil
+  end
+
 end
